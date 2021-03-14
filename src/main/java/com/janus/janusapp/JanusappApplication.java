0e5fcd681a580962730f85e0327e7f056ae5f2d9
Janusred <horaciok300@hotmail.com>
2021-03-13 23:35:11 -0600

    Inicio

---
+++ src/main/java/com/janus/janusapp/JanusappApplication.java
@@ -0,0 +1,13 @@
+package com.janus.janusapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JanusappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JanusappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/janus/janusapp/JanusappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janus.janusapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JanusappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
