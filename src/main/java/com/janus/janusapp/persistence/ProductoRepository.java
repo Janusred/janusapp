26b8e1f3f5a80e8dc66cb6ff8825d46f64158270
Janusred <horaciok300@hotmail.com>
2021-03-23 00:00:14 -0600

    QueryMetodos

--- src/main/java/com/janus/janusapp/persistence/ProductoRepository.java
+++ src/main/java/com/janus/janusapp/persistence/ProductoRepository.java
@@ -3,11 +3,19 @@ import com.janus.janusapp.persistence.crud.ProductoCrudRepository;
 import com.janus.janusapp.persistence.entity.Producto;
 
 import java.util.List;
+import java.util.Optional;
 
 public class ProductoRepository {
     private ProductoCrudRepository productoCrudRepository;
 
     public List<Producto> getAll(){
         return (List<Producto>) productoCrudRepository.findAll();
+    }
+    public List<Producto> getByCategoria(int idCategoria){
+        return  productoCrudRepository.findByIdCategoria(idCategoria);
+
+    }
+    public Optional<List<Producto>> getEscasos(int cantidad){
+        return productoCrudRepository.findByCantidadStocklessThanAndEstado(cantidad, true);
     }
 }
--- src/main/java/com/janus/janusapp/persistence/crud/ProductoCrudRepository.java
+++ src/main/java/com/janus/janusapp/persistence/crud/ProductoCrudRepository.java
@@ -3,5 +3,11 @@ package com.janus.janusapp.persistence.crud;
 import com.janus.janusapp.persistence.entity.Producto;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ProductoCrudRepository extends CrudRepository<Producto,Integer> {
+
+    List<Producto> findByIdCategoria(int idCategoria);
+    Optional<List<Producto>> findByCantidadStocklessThanAndEstado(int cantidadStock, boolean estado);
 }
