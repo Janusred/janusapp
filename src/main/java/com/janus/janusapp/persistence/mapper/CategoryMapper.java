c5fb52f7bf7d7456359502c451abd14488725b84
Janusred <horaciok300@hotmail.com>
2021-03-23 01:23:04 -0600

    Mapping

---
+++ src/main/java/com/janus/janusapp/persistence/mapper/CategoryMapper.java
@@ -0,0 +1,24 @@
+package com.janus.janusapp.persistence.mapper;
+
+import com.janus.janusapp.domain.Category;
+import com.janus.janusapp.persistence.entity.Categoria;
+import org.mapstruct.InheritInverseConfiguration;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.Mappings;
+
+@Mapper(componentModel = "spring")
+public interface CategoryMapper {
+    @Mappings(
+            {
+                    @Mapping(source = "idCategoria", target = "categoryId"),
+                    @Mapping(source = "descripcion", target = "category"),
+                    @Mapping(source = "estado", target = "active"),
+            }
+    )
+    Category toCategory(Categoria categoria);
+
+    @InheritInverseConfiguration
+    @Mapping(target = "producto", ignore = true)
+    Categoria toCategoria(Category category);
+}
