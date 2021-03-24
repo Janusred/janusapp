670a3e2643089be44cecf271060d0b4dff0c16d3
Janusred <horaciok300@hotmail.com>
2021-03-24 14:53:59 -0600

    Purchase

---
+++ src/main/java/com/janus/janusapp/domain/repository/PurchaseRepository.java
@@ -0,0 +1,12 @@
+package com.janus.janusapp.domain.repository;
+
+import com.janus.janusapp.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
